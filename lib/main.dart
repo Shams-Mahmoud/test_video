386dad9daa822b050e4a8698992ce2c147f0f9c2
shams-mahmoud <sh.mahmoud@glcpaints.com>
2024-03-18 12:50:59 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home:  _ButterFlyAssetVideo(),
+    );
+  }
+}
+
+
+class _ButterFlyAssetVideo extends StatefulWidget {
+  @override
+  _ButterFlyAssetVideoState createState() => _ButterFlyAssetVideoState();
+}
+
+class _ButterFlyAssetVideoState extends State<_ButterFlyAssetVideo> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('videos/Main.mp4');
+
+    _controller.addListener(() {
+      setState(() {
+        if(_controller.value.isCompleted){
+          _controller = VideoPlayerController.asset('videos/Connect.mp4');
+          _controller.initialize().then((_) => setState(() {}));
+          _controller.play();
+
+        }
+
+      });
+    });
+     _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  VideoPlayer(_controller),
+    );
+  }
+}
